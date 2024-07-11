fccb653469cc3c656a147924fea0a6fcd8d06b0d
Justmiron007 <raen.96@mail.ru>
2024-07-12 00:21:34 +0500

    Add files via upload

---
+++ module_5_4.py
@@ -0,0 +1,80 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args):
+        cls.args = args
+        cls.houses_history.append(args[0])
+        return super().__new__(cls)
+
+    def __del__(self):
+        print(f'{self.name} снесён, но он останется в истории')
+
+    def __init__(self, name, number):
+        self.name = name
+        self.number_of_floors = number
+
+    def go_to(self, new_floor):
+        for i in range(1, new_floor + 1):
+            if 1 <= new_floor <= self.number_of_floors:
+                print(i)
+            else:
+                print('Такого этажа не существует')
+                break
+
+    def int_(other, int):
+        if isinstance(other, int):
+            return True
+
+    def house_(other, House):
+        if isinstance(other, House):
+            return True
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название:{self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        return self.number_of_floors == other
+
+    def __lt__(self, other):
+        return self.number_of_floors < other
+
+    def __le__(self, other):
+        return self.number_of_floors <= other
+
+    def __gt__(self, other):
+        return self.number_of_floors > other
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other
+
+    def __ne__(self, other):
+        return self.number_of_floors != other
+
+    def __add__(self, value):
+        self.number_of_floors += value
+        return self
+
+    def __radd__(self, value):
+        self.number_of_floors += value
+        return self
+
+    def __iadd__(self, value):
+        self.number_of_floors += value
+        return self
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
